db7b7610d7707e3911a21914b043b2517d57487a
Aditya Prayoga <aditya@brainmatics.com>
2024-09-22 15:56:46 +0700

    first commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    function test_that_false_is_false() {
+        $this->assertFalse(false);
+    }
+}
