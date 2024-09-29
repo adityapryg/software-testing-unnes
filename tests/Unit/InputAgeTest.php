888a0afad1ec3594254c39cabfea7870a3f77f38
Aditya Prayoga <aditya@brainmatics.com>
2024-09-29 16:19:03 +0700

    add withoutmiddleware method

--- tests/Unit/InputAgeTest.php
+++ tests/Unit/InputAgeTest.php
@@ -14,13 +14,13 @@ pest()->extend(Tests\TestCase::class);
 
 it('success to access input age page', function () {
     //akses halaman input-age
-    $response = $this->get('/input-age');
+    $response = $this->withoutMiddleware()->get('/input-age');
     //coba cek jika halaman berhasil diakses
     $response->assertStatus(200);
 });
 
 it('success to input age between 20 to 50', function () {
-    $response = $this->post('/input-age', ['age' => 40]);
+    $response = $this->withoutMiddleware()->post('/input-age', ['age' => 40]);
 
     $response->assertStatus(200);
     expect($response->json('message'))->toBe('Age successfully submitted!');
@@ -28,25 +28,25 @@ it('success to input age between 20 to 50', function () {
 });
 
 it('fails when age is less than 20', function () {
-    $response = $this->post('/input-age', ['age' => 10]);
+    $response = $this->withoutMiddleware()->post('/input-age', ['age' => 10]);
 
     $response->assertInvalid(['age' =>'The age field must be between 20 and 50.']);
 });
 
 it('fails when age is greater than 50', function () {
-    $response = $this->post('/input-age', ['age' => 60]);
+    $response = $this->withoutMiddleware()->post('/input-age', ['age' => 60]);
 
     $response->assertInvalid(['age' =>'The age field must be between 20 and 50.']);
 });
 
 it('fails when age is not an integer', function () {
-    $response = $this->post('/input-age', ['age' => 30.4]);
+    $response = $this->withoutMiddleware()->post('/input-age', ['age' => 30.4]);
 
     $response->assertInvalid(['age' =>'The age field must be an integer.']);
 });
 
 it('fails when age is a non-numeric character', function () {
-    $response = $this->post('/input-age', ['age' => 'unnes']);
+    $response = $this->withoutMiddleware()->post('/input-age', ['age' => 'unnes']);
 
     $response->assertInvalid(['age' => 'The age field must be a number.']);
 });
